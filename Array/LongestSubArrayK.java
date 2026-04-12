eec3b1c2315851526eb13ec53160fb02ea4816de
SansuKumar <sansukumar57@gmail.com>
2026-04-12 10:57:24 +0530

    DSA

--- Array/LongestSubArrayK.java
+++ Array/LongestSubArrayK.java
@@ -1,5 +1,20 @@
 package DSA.Array;
 
 public class LongestSubArrayK {
+    public int longestSubarray(int[] arr, int k) {
+        // code here
+        int maxLen=0;
+        for(int i=0;i<arr.length;i++){
+            int sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum+=arr[j];
+                if(sum==k){
+                    int len=j-i+1;
+                    maxLen=Math.max(maxLen,len);
+                }
+            }
+        }
+        return maxLen;
+    }
 
 }
