09da492c2f56eb5408ae05117b1c298248a99ec5
SansuKumar <sansukumar57@gmail.com>
2026-04-13 09:16:43 +0530

    DSA

---
+++ Array/RemoveElem.java
@@ -0,0 +1,19 @@
+package DSA.Array;
+
+class RemoveElem {
+    public int removeElement(int[] nums, int val) {
+
+        int count=0;
+for(int i=0;i<nums.length;i++){
+    if(nums[i]!=val){
+   nums[count]= nums[i];
+   count++;
+    }
+
+}
+
+
+    return count;
+    }
+    
+}
