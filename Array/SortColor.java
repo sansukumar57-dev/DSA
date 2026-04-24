e346651b6b7ecb2dfc03413854605c2712d94ca6
SansuKumar <sansukumar57@gmail.com>
2026-04-24 18:59:53 +0530

    DSA

---
+++ Array/SortColor.java
@@ -0,0 +1,15 @@
+package DSA.Array;
+
+class SortColor {
+    public void sortColors(int[] nums) {
+      for(int i=0;i<nums.length;i++){
+        for(int j=i;j<nums.length;j++){
+            if(nums[i]>nums[j]){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+        }
+      }  
+    }
+}
