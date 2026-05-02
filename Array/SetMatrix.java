9170b208de2baf59f50bac939b3e3233e8dda3c7
SansuKumar <sansukumar57@gmail.com>
2026-05-02 15:52:45 +0530

    DSA

---
+++ Array/SetMatrix.java
@@ -0,0 +1,38 @@
+package DSA.Array;
+
+import java.util.*;
+
+class SetMatrix {
+    public void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        List<Integer> rows = new ArrayList<>();
+        List<Integer> cols = new ArrayList<>();
+
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+
+     
+        for (int r : rows) {
+            for (int j = 0; j < n; j++) {
+                matrix[r][j] = 0;
+            }
+        }
+
+       
+        for (int c : cols) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][c] = 0;
+            }
+        }
+    }
+}
