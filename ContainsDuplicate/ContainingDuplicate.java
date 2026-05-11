5939b2709752b30b87479544759bb0cd9a5c06d1
SansuKumar <sansukumar57@gmail.com>
2026-05-11 16:41:02 +0530

    DSA

---
+++ ContainsDuplicate/ContainingDuplicate.java
@@ -0,0 +1,20 @@
+package DSA.ContainsDuplicate;
+
+import java.util.HashSet;
+
+class ContainingDuplicate {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length;i++) {
+            if(set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            System.out.println(set.size());
+            if(set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false; 
+    }
+}
