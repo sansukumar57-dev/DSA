81ef05b546f1e4b084271ada181ec9a3e5a98a57
SansuKumar <sansukumar57@gmail.com>
2026-05-15 17:52:38 +0530

    DSA

--- BinarySearch/InsertionSort.java
+++ BinarySearch/InsertionSort.java
@@ -1,23 +1,20 @@
 package DSA.BinarySearch;
 
 class InsertionSort {
-    public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        public int searchInsert(int[] nums, int target) {
+            int left = 0, right = nums.length;
 
-            if (nums[mid] == target) {
-                return mid;
-            } 
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            } 
-            else {
-                right = mid - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
             }
+
+            return left;
         }
- return left;
     }
-}
