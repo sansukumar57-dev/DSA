f38fc2f84e7f0a7819d6f095a1b91b4fcb7dcaad
SansuKumar <sansukumar57@gmail.com>
2026-05-22 14:33:30 +0530

    DSA

---
+++ Array/SquareSortedArray.java
@@ -0,0 +1,11 @@
+package DSA.Array;
+
+class SquareSortedArray {
+    public int[] sortedSquares(int[] nums) {
+         for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
