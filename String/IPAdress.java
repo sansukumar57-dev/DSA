dc14b9457ddbe221e4dac1d552d158c645939596
SansuKumar <sansukumar57@gmail.com>
2026-06-23 18:54:04 +0530

    DSA

---
+++ String/IPAdress.java
@@ -0,0 +1,15 @@
+package DSA.String;
+
+class IPAdress {
+    public String defangIPaddr(String address) {
+         String ans="";
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)=='.'){
+                ans+="[.]";
+            }
+            else
+            ans+=address.charAt(i);
+        }
+        return ans;
+    }
+}
