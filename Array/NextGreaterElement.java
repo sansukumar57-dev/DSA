d77fe9b00d2ec8c86f83ebe6ac5924db645ec0a6
SansuKumar <sansukumar57@gmail.com>
2026-06-30 18:22:02 +0530

    DSA

---
+++ Array/NextGreaterElement.java
@@ -0,0 +1,29 @@
+package DSA.Array;
+
+class NextGreaterElement {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+         int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+
+            int j = 0;
+
+            while (nums2[j] != nums1[i]) {
+                j++;
+            }
+
+            int next = -1;
+
+            for (int k = j + 1; k < nums2.length; k++) {
+                if (nums2[k] > nums2[j]) {
+                    next = nums2[k];
+                    break;
+                }
+            }
+
+            ans[i] = next;
+        }
+
+        return ans;
+    }
+}
