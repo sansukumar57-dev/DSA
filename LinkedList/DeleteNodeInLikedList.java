28119e41d85b29647a06421115b780569982bb53
Sansukumar <sansukumar57@gmail.com>
2026-07-13 18:05:28 +0530

    DSA

---
+++ LinkedList/DeleteNodeInLikedList.java
@@ -0,0 +1,16 @@
+package DSA.LinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class DeleteNodeInLikedList {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
+}
