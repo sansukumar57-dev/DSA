65efbaad8521600f920c6e1c13a40c0a27c402f0
Sansukumar <sansukumar57@gmail.com>
2026-08-04 19:50:40 +0530

    DSA

--- Array/MaxSubArray.java
+++ Array/MaxSubArray.java
@@ -3,11 +3,11 @@ package DSA.Array;
 class MaxSubArray {
     public int maxSubArray(int[] nums) {
         int sum=nums[0];
-        int msum=nums[0];
+        int Msum=nums[0];
         for(int i=1;i<nums.length;i++){
             sum=Math.max(nums[i],sum+nums[i]);
-            msum=Math.max(msum,sum);
+            Msum=Math.max(Msum,sum);
         }
-        return msum;
+        return Msum;
     }
 }
