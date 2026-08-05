8f69664cbd86a0f5db3149e62bfc4d6db1156557
Sansukumar <sansukumar57@gmail.com>
2026-08-05 18:05:26 +0530

    DSA

---
+++ Array/RearrangeArray.java
@@ -0,0 +1,20 @@
+package DSA.Array;
+
+class RearrangeArray {
+    public int[] rearrangeArray(int[] nums) {
+        int[] arr=new int[nums.length];
+        int pos=0,neg=1;
+
+        for(int num:nums){
+            if(num>0){
+                arr[pos]=num;
+                pos+=2;
+            }
+            else{
+                arr[neg]=num;
+                neg+=2;
+            }
+        }
+        return arr;
+    }
+}
