b60ec6b342cfbf4dff525d05d5d119ce553e4b53
Sansukumar <sansukumar57@gmail.com>
2026-08-08 18:36:54 +0530

    DSA

---
+++ TwoPointer/MergeIntervals.java
@@ -0,0 +1,24 @@
+package DSA.TwoPointer;
+
+class MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+
+
+     Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0]));
+
+     List<int[]>res=new ArrayList<>();
+     res.add(intervals[0]);
+    
+    for(int i=1;i<intervals.length;i++){
+            int[] last=res.get(res.size()-1);
+            if(intervals[i][0]<=last[1]){
+                last[1]=Math.max(last[1],intervals[i][1]);
+            }
+            else{
+                res.add(intervals[i]);
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+
+    }
+}
