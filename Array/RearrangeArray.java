8f1c9ed6210f2729ade629cb203bf702c9263138
Sansukumar <sansukumar57@gmail.com>
2026-08-20 18:11:27 +0530

    DSA

--- Array/RearrangeArray.java
+++ Array/RearrangeArray.java
@@ -2,18 +2,22 @@ package DSA.Array;
 
 class RearrangeArray {
     public int[] rearrangeArray(int[] nums) {
-        int[] arr=new int[nums.length];
-        int pos=0,neg=1;;
+       List<Integer> pos=new ArrayList<>();
+       List<Integer> neg=new ArrayList<>();
+       for(int num:nums){
+        if(num>0){
+            pos.add(num);
+        }else{
+            neg.add(num);
+        }
+       }
+       int[] result = new int[nums.length];
 
-        for(int num:nums){
-            if(num>0){
-                arr[pos]=num;
-                pos+=2;
-            }
-            else{
-                arr[neg]=num;
-                neg+=2;
-            }
+        int index = 0;
+
+        for (int i = 0; i < positive.size(); i++) {
+            result[index++] = positive.get(i);
+            result[index++] = negative.get(i);
         }
         return arr;
     }
