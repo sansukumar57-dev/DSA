8146487c03c07236bd3a9173d29cca410a2d3f37
Sansukumar <sansukumar57@gmail.com>
2026-08-31 18:48:39 +0530

    DSA

---
+++ src/DSA/BitManauplation/ReverseBit.java
@@ -0,0 +1,18 @@
+package DSA.BitManauplation;
+
+class ReverseBit {
+    public int reverseBits(int n) {
+       int result = 0;
+
+    for (int i = 0; i < 32; i++) {
+        int bit = n & 1;
+
+        result = result << 1;
+        result = result | bit;
+
+        n = n >>> 1;
+    }
+
+    return result;
+    }
+}
