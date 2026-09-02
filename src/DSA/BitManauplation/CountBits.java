cbf8dacbd423866214f60095dd0cb0d0a0a82b56
Sansukumar <sansukumar57@gmail.com>
2026-09-02 18:08:47 +0530

    DSA

--- src/DSA/BitManauplation/CountBits.java
+++ src/DSA/BitManauplation/CountBits.java
@@ -5,7 +5,7 @@ class CountBits {
         int[] ans = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i >> 1] + (i & 1);
+            ans[i] =ans[i >> 1] + (i & 1);
         }
 
         return ans;
---
+++ src/DSA/LinkedList/ReorderList.java
@@ -0,0 +1,41 @@
+package DSA.LinkedList;
+
+class ReorderList {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = slow.next;
+        slow.next = null;
+
+        ListNode prev = null;
+
+        while (second != null) {
+            ListNode temp = second.next;
+            second.next = prev;
+            prev = second;
+            second = temp;
+        }
+
+        ListNode first = head;
+        second = prev;
+
+        while (second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+    }
+}
+}
