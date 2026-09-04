27f10136012218ef9788b21f56e18c370586c80f
Sansukumar <sansukumar57@gmail.com>
2026-09-04 19:13:11 +0530

    DSA

---
+++ src/DSA/BinarySearch/BinarySearchTree.java
@@ -0,0 +1,16 @@
+package DSA.BinarySearch;
+
+class BinarySearchTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root!=null){
+            if(root.val==val){
+                return root;
+            }else if(val<root.val){
+                root= root.left;
+            }else{
+                root= root.right;
+            }
+        }
+        return null;
+    }
+}
