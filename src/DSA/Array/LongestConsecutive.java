d5c538e13c2e22904469f7b71cbe2cd78420a7d9
Sansukumar <sansukumar57@gmail.com>
2026-09-10 18:06:36 +0530

    DSA

---
+++ src/DSA/Array/LongestConsecutive.java
@@ -0,0 +1,31 @@
+package DSA.Array;
+
+class LongestConsecutive {
+    public int longestConsecutive(int[] nums) {
+           if (nums.length == 0) return 0;
+
+        Arrays.sort(nums);
+
+        int longest = 1;
+        int current = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] == nums[i - 1]) {
+               
+                continue;
+            }
+
+            if (nums[i] == nums[i - 1] + 1) {
+                current++;
+            } else {
+                current = 1;
+            }
+
+            longest = Math.max(longest, current);
+        }
+
+        return longest;
+    
+    }
+}
