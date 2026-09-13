c7dba9faeeecc5c44c15f6aa54c39f4b8cc119fd
Sansukumar <sansukumar57@gmail.com>
2026-09-13 15:04:39 +0530

    DSA

--- src/DSA/Array/BestTimeToSell.java
+++ src/DSA/Array/BestTimeToSell.java
@@ -5,7 +5,7 @@ class BestTimeToSell {
        int minPrice = prices[0];
         int answer = 0;
 
-        for (int i = 1; i < prices.length; i++) {
+        for (int i =1;i< prices.length; i++) {
             answer = Math.max(answer, prices[i] - minPrice);
             minPrice = Math.min(minPrice, prices[i]);
         }
