2a923f0a321929f37d8564c434b14b7d626ddd39
Sansukumar <sansukumar57@gmail.com>
2026-09-14 18:01:29 +0530

    DSA

--- src/DSA/Array/NextPermutation.java
+++ src/DSA/Array/NextPermutation.java
@@ -1,42 +1,31 @@
 package DSA.Array;
 
+import java.util.Arrays;
+
 class NextPermutation {
-
     public void nextPermutation(int[] nums) {
-
         int n = nums.length;
-
         int i = n - 2;
 
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
-        if (i >= 0) {
 
-            int j = n - 1;
-
-            while (nums[j] <= nums[i]) {
-                j--;
-            }
-
-            swap(nums, i, j);
+        if (i == -1) {
+            Arrays.sort(nums);
+            return;
         }
-        reverse(nums, i + 1, n - 1);
-    }
-    private void swap(int[] nums, int i, int j) {
+
+        int j = n - 1;
+
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
 
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-    }
-    private void reverse(int[] nums, int left, int right) {
 
-        while (left < right) {
-
-            swap(nums, left, right);
-
-            left++;
-            right--;
-        }
+        Arrays.sort(nums, i + 1, n);
     }
 }
