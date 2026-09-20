97c89fecee7c4ccdc4e376c730ca8c006104d4b9
Sansukumar <sansukumar57@gmail.com>
2026-09-20 18:42:23 +0530

    DSA

---
+++ src/DSA/Array/RemoveDuplicate.java
@@ -0,0 +1,16 @@
+package DSA.Array;
+
+class RemoveDuplicate {
+    public int removeDuplicates(int[] nums) {
+        
+     int k = 0;
+
+        for (int num : nums) {
+            if (k < 2 || num != nums[k - 2]) {
+                nums[k] = num;
+                k++;
+            }
+        }
+
+        return k;
+}}
