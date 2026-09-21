447cdf16fc230c4e5c00c4f0154f3c6cb754b3b0
Sansukumar <sansukumar57@gmail.com>
2026-09-21 19:39:40 +0530

    DSA

--- src/DSA/Array/RemoveDuplicate.java
+++ src/DSA/Array/RemoveDuplicate.java
@@ -2,13 +2,21 @@ package DSA.Array;
 
 class RemoveDuplicate {
     public int removeDuplicates(int[] nums) {
-        
-     int k = 0;
+
+        int k = 0;
+        int count = 0;
+        int prev = Integer.MIN_VALUE;
 
         for (int num : nums) {
-            if (k < 2 || num != nums[k - 2]) {
-                nums[k] = num;
-                k++;
+            if (num == prev) {
+                count++;
+            } else {
+                prev = num;
+                count = 1;
+            }
+
+            if (count <= 2) {
+                nums[k++] = num;
             }
         }
 
