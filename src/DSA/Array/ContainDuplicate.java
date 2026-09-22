a8827a7d73588990939713c1f7feb3fe0d889ba5
Sansukumar <sansukumar57@gmail.com>
2026-09-22 18:16:45 +0530

    DSA

---
+++ src/DSA/Array/ContainDuplicate.java
@@ -0,0 +1,20 @@
+package DSA.Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class ContainDuplicate {
+    public boolean containsDuplicate(int[] arr) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : arr) {
+            if (seen.contains(num)) {
+                return true;
+            }
+
+            seen.add(num);
+        }
+
+        return false;
+    }
+}
